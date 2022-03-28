d3e7cb153c124bcf5ae30f445f837898c646f889
Ysedda <86974668+Ysedda@users.noreply.github.com>
2022-03-28 10:55:12 +0200

    Add files via upload

---
+++ OOP2truck.php
@@ -0,0 +1,47 @@
+<?php
+
+require_once 'OOP2.php';
+
+class Truck extends Vehicle
+{
+    private int $capacity;
+
+    private int $cargo;
+
+    public function __construct(string $color, int $nbSeats,string $energy, int $cargo)
+    {
+        parent::__construct($color, $nbSeats, $energy);
+        $this->cargo = $cargo;      
+    }
+
+    public function getCapacity():int
+    {
+        return $this->capacity;
+    }
+
+    public function setCapacity(int $capacity):void
+    {
+        $this->capacity = $capacity;
+    }
+
+    public function getCargo():int
+    {
+        return $this->cargo;
+    }
+
+    public function setCargo(int $cargo):void
+    {
+        $this->cargo = $cargo;
+    }
+    
+    public function isFull():string
+    {
+        $sentence = '';
+        if ($this->cargo === $this->capacity) {
+            $sentence .= 'full';
+        } elseif ($this->cargo < $this->capacity) {
+            $sentence .= 'in filling';
+        }
+        return $sentence;
+    }
+}
