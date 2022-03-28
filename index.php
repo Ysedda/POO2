f7353b11b4a41cda0b37b8717b5c7a681da3f4a2
Ysedda <86974668+Ysedda@users.noreply.github.com>
2022-03-28 11:34:50 +0200

    Update index.php

--- index.php
+++ index.php
@@ -2,3 +2,10 @@
 
 requireonce 'Vehicle.php';
 requireonce 'Truck.php';
+$bigDaddy = new Truck('blue', 2, 'oil', 500);
+var_dump($bigDaddy);
+$bigDaddy->setCargo(600);
+$bigDaddy->forward();
+$bigDaddy->brake();
+$bigDaddy->isFull();
+var_dump($bigDaddy);
